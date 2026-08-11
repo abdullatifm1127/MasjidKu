41ccfdf69615512649d732789ec12646b4eaf21e
= <=>
2026-08-11 11:14:30 +0700

    halaman utama

--- MasjidAnnur/temp-masjid/routes-web.php
+++ MasjidAnnur/temp-masjid/routes-web.php
@@ -2,22 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Login
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-// Register
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
 
-// Forgot Password
 Route::get('/forgot-password', function () {
     return view('auth.login');
 })->name('password.request');
