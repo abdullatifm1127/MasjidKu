dbb91c5c3a5355e76ef7eae84527180a9224b02d
= <=>
2026-08-13 10:29:33 +0700

    p

--- MasjidAnnur/routes/web.php
+++ MasjidAnnur/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MosqueController;
+=======
+>>>>>>> 7fcff53fb5b947e1a2e47bffc9f273956b98d171
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +34,8 @@ Route::get('/login', function () {
 Route::post('/login', [AuthController::class, 'login'])
     ->name('login.process');
 
+=======
+>>>>>>> 7fcff53fb5b947e1a2e47bffc9f273956b98d171
 // Halaman Daftar Akun
 Route::get('/register', function () {
     return view('auth.registerAkun');
@@ -73,3 +78,31 @@ Route::middleware('auth')->group(function () {
 Route::get('/forgot-password', function () {
     return view('auth.login'); // Ubah jika nanti sudah membuat view khusus forgot password
 })->name('password.request');
+=======
+// Halaman Daftarkan Masjid
+Route::get('/daftar-masjid', function () {
+    return view('auth.registerMasjid');
+})->name('daftar.masjid');
+
+// Forgot Password
+Route::get('/forgot-password', function () {
+    return view('auth.login');
+})->name('password.request');
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
+
+// Dashboard Admin
+Route::get('/admin/dashboard', function () {
+    return view('auth.berandaAdmin');
+})->name('admin.dashboard');
+
+// Landing Page Editor
+Route::get('/admin/landing-page', function () {
+    return view('admin.landingPage');
+})->name('admin.landing-page');
+>>>>>>> 7fcff53fb5b947e1a2e47bffc9f273956b98d171
