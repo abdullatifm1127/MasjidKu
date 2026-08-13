7fcff53fb5b947e1a2e47bffc9f273956b98d171
abdullatifm1127 <bdllm1104@gmail.com>
2026-08-13 10:07:08 +0700

    Halaman Untuk Admin

--- MasjidAnnur/routes/web.php
+++ MasjidAnnur/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\MosqueController;
+
 /*
 |--------------------------------------------------------------------------
 | Halaman Utama
@@ -25,53 +24,34 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-// Proses Login
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])
-    ->name('login.process');
-
 // Halaman Daftar Akun
 Route::get('/register', function () {
     return view('auth.registerAkun');
 })->name('register');
 
-// Proses Daftar Akun
-Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])
-    ->name('register.process');
+// Halaman Daftarkan Masjid
+Route::get('/daftar-masjid', function () {
+    return view('auth.registerMasjid');
+})->name('daftar.masjid');
 
-// Logout
-Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])
-    ->name('logout');
-
-
-/*
-|--------------------------------------------------------------------------
-| Halaman yang membutuhkan Login
-|--------------------------------------------------------------------------
-*/
-
-Route::middleware('auth')->group(function () {
-
-    // Form Daftarkan Masjid
-    Route::get('/daftar-masjid', [MosqueController::class, 'create'])
-        ->name('daftar.masjid');
-
-    // Proses simpan masjid
-    Route::post('/daftar-masjid', [MosqueController::class, 'store'])
-        ->name('daftar.masjid.store');
-
-    // Dashboard
-    Route::get('/dashboard', [MosqueController::class, 'dashboard'])
-        ->name('dashboard');
-
-});
-
-
-/*
-|--------------------------------------------------------------------------
-| Forgot Password
-|--------------------------------------------------------------------------
-*/
-
+// Forgot Password
 Route::get('/forgot-password', function () {
     return view('auth.login');
 })->name('password.request');
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
+
+// Dashboard Admin
+Route::get('/admin/dashboard', function () {
+    return view('auth.berandaAdmin');
+})->name('admin.dashboard');
+
+// Landing Page Editor
+Route::get('/admin/landing-page', function () {
+    return view('admin.landingPage');
+})->name('admin.landing-page');
